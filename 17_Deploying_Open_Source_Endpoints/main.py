e751a07d636380f5e5bf1266ac1692d87fddf66a
Keertana <keertana.chandar@gmail.com>
2025-11-08 13:36:01 -0800

    start of session 17 hw

--- 17_Deploying_Open_Source_Endpoints/main.py
+++ 17_Deploying_Open_Source_Endpoints/main.py
@@ -1,6 +1,14 @@
 def main():
     print("Hello from 17-deploying-open-source-endpoints!")
 
+# import getpass
+# import os
+
+# if "TOGETHER_API_KEY" not in os.environ:
+#     os.environ["TOGETHER_API_KEY"] = getpass.getpass("Enter your Together API key: ")
+
+# os.environ["LANGSMITH_API_KEY"] = getpass.getpass("Enter your LangSmith API key: ")
+# os.environ["LANGSMITH_TRACING"] = "true"
 
 if __name__ == "__main__":
     main()
